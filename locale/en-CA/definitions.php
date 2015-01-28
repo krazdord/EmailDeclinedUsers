8cac4b252843beba390470c8b354420c2fb465cc
krazdord <krazdord@gmail.com>
2015-01-28 16:45:49 -0500

    locale not needed anymore, remove

--- locale/en-CA/definitions.php
+++
@@ -1,5 +0,0 @@
-<?php if (!defined('APPLICATION')) exit();
-$Definition['EmailMembershipDeclined'] = 'Hello %1$s,
-
-We are sorry to inform you that you were declined for Ex Inferno membership. Please review your application and resubmit it with more information. Thanks!';
-
